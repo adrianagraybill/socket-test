2c2c6914229dd6c985da0c62358aede4b2ddafd0
adrianagraybill <adriana.graybill@gmail.com>
2019-08-17 14:00:31 -0700

    done

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 const io = require('socket.io-client');
 
-const socket = io.connect('http://172.16.0.231:3000');
+const socket = io.connect('http://localhost:3000');
 
 const faker = require('faker');
 
 setInterval( () => {
   socket.emit('speak', faker.hacker.phrase());
-},1000);
+},3000);
--- server.js
+++ server.js
@@ -17,5 +17,3 @@ io.on('connection', (socket) => {
   })
 
 });
-
-
